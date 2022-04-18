3d79b65da88f1d5ab29f98bd701f723824323a7c
AjisaiHachimitsu <daigan47@gmail.com>
2022-04-19 01:35:12 +0900

    チュートリアル終わり

--- src/ToDoInputForm.svelte
+++ src/ToDoInputForm.svelte
@@ -1,5 +1,22 @@
 <script lang='ts'>
+  import {toDoItems} from './store';
+  import {onMount} from 'svelte';
     export let userName:string
+    let newItem="";
+    let newItemInputForm=null;
+
+    onMount(()=>{
+      newItemInputForm.focus();
+    })
+
+    function addToList()
+    {
+      $toDoItems=[...$toDoItems,{text: newItem,status: false}];
+      newItem="";
+    }
   </script>
+
+  <input bind:value={newItem} bind:this={newItemInputForm} type="text" placeholder="新しく追加するタスク">
+  <button on:click={addToList}>追加</button>
   
   <h3>{userName}のタスクリスト:</h3>
--- src/ToDoList.svelte
+++ src/ToDoList.svelte
@@ -1,17 +1,13 @@
 <script>
-    let toDoItems = [
-                      {text: 'ゴミ出し', status: true},
-                      {text: 'プログラミング学習', status: false},
-                      {text: '友達に連絡', status: false}
-                    ];
+    import {toDoItems} from './store';
   
     function removeFromList(i) {
-      toDoItems.splice(i, 1)
-      toDoItems = toDoItems
+      $toDoItems.splice(i, 1)
+      $toDoItems = $toDoItems
     }
 </script>
 
-  {#each toDoItems as item,index}
+  {#each $toDoItems as item,index}
     <div class="toDoItems">
         <input bind:checked={item.status} type="checkbox">
      <span class:checked={item.status}>{item.text}</span>
